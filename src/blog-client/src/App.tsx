0fead6aca3ae5eb5412b58148e1c376971e86a39
ShadyObeyd <obeyd.shady@gmail.com>
2020-07-20 12:36:36 +0300

    Created Footer component.

--- src/blog-client/src/App.tsx
+++ src/blog-client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to the blogging system!</h1>
       <hr />
+      <Footer />
     </div>
   );
 }
