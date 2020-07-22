1ce36255fbea6e7b5b283b74d5a6e395236e4c2a
ShadyObeyd <obeyd.shady@gmail.com>
2020-07-22 13:38:23 +0300

    Replaced a tag with Link in NavLink component.

--- src/blog-client/src/components/NavLink/NavLink.jsx
+++ src/blog-client/src/components/NavLink/NavLink.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function NavLink({ text }) {
     return (
-        <a href="#">{text}</a>
+        <Link to="#">{text}</Link>
     );
 }
 
