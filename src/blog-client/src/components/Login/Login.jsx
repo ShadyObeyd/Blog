2026d74229fc8a5fb961575e0cd1292f2909d770
ShadyObeyd <obeyd.shady@gmail.com>
2020-07-23 21:57:09 +0300

    Added validation to Login component.

--- src/blog-client/src/components/Login/Login.jsx
+++ src/blog-client/src/components/Login/Login.jsx
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Login.module.css';
 import { Link } from 'react-router-dom';
 import Button from '../Button/Button';
+import Error from '../Error/Error';
+import { emailPattern, invalidEmailMessage, passwordMinLength, invalidPasswordMessage } from '../../constants';
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [formIsValid, setFormIsValid] = useState(true);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    function handleEmailChange(event) {
+        setEmail(event.target.value);
+    }
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value);
+    }
+
     function buttonClicked(event) {
         event.preventDefault();
-        console.log('Click from Login');
+
+        if (!email.match(emailPattern)) {
+           setFormIsValid(false);
+           setErrorMessage(invalidEmailMessage);
+           return; 
+        }
+
+        if (password.length < passwordMinLength) {
+            setFormIsValid(false);
+            setErrorMessage(invalidPasswordMessage);
+            return;
+        }
+
+        // TODO Make request to back-end
+    }
+
+    if (!formIsValid) {
+        setTimeout(() => {
+            setFormIsValid(true);
+        }, 4000);
     }
 
     return (
         <div className={styles.login}>
+            {!formIsValid ? <Error errorMessage={errorMessage} /> : null}
             <form>
                 <label htmlFor="email">Email:</label>
-                <input id="email" type="email" placeholder="Email..." />
+                <input id="email" type="email" onChange={handleEmailChange} placeholder="Email..." />
                 <label htmlFor="password">Password:</label>
-                <input id="password" type="password" placeholder="Password..." />
+                <input id="password" type="password" onChange={handlePasswordChange} placeholder="Password..." />
                 <br />
                 <Button clicked={buttonClicked} text="Login"/>
             </form>
