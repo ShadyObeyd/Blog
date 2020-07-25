133cc8bbf02cb062cc5cbef535057eb17eacded0
ShadyObeyd <obeyd.shady@gmail.com>
2020-07-25 15:39:43 +0300

    Removed validation attributes to request models.

--- src/BlogServer/BlogServer.Models/RequestModels/Users/LoginModel.cs
+++ src/BlogServer/BlogServer.Models/RequestModels/Users/LoginModel.cs
@@ -5,14 +5,8 @@ namespace BlogServer.Models.RequestModels.Users
 {
     public class LoginModel
     {
-        private const int MinLength = 3;
-
-        [EmailAddress(ErrorMessage = Constants.InvalidEmailMessage)]
-        [Required]
         public string Email { get; set; }
 
-        [Required]
-        [MinLength(MinLength, ErrorMessage = Constants.PasswordTooShortMessage)]
         public string Password { get; set; }
     }
 }
--- src/BlogServer/BlogServer.Models/RequestModels/Users/RegisterModel.cs
+++ src/BlogServer/BlogServer.Models/RequestModels/Users/RegisterModel.cs
@@ -5,17 +5,10 @@ namespace BlogServer.Models.RequestModels.Users
 {
     public class RegisterModel
     {
-        private const int MinLength = 3;
-
-        [EmailAddress(ErrorMessage = Constants.InvalidEmailMessage)]
-        [Required]
         public string Email { get; set; }
 
-        [Required]
-        [MinLength(MinLength, ErrorMessage = Constants.PasswordTooShortMessage)]
         public string Password { get; set; }
 
-        [Compare(nameof(Password), ErrorMessage = Constants.PasswordsDontMatchMessage)]
         public string RepeatPassword { get; set; }
     }
 }
